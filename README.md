4e2ef27d608e51654b8a13d9a9547acc26224e65
John Drogo <admin@jldapps.com>
2014-06-05 10:22:29 -0400

    More repository fixes.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ CrawlingSpecter
 ===============
 
 Simple web crawler using Spooky and NodeJS.
+Run with node index.js
+
+Start url is hardcoded.
+It defaults to the first value of the queue.
--- index.js
+++ index.js
@@ -5,78 +5,97 @@ try {
 }
 
 console.log("Hi.")
-rooturl = "http://www.w3schools.com/jsref/jsref_indexof.asp"
 
-var spooky = new Spooky({
-        child: {
-            transport: 'http'
-        },
-        casper: {
-            clientScripts: [ "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"],
-            logLevel: 'debug',
-            verbose: true
-        }
-    }, function (err) {
-        if (err) {
-            e = new Error('Failed to initialize SpookyJS');
-            e.details = err;
-            console.log("Error, could not initialize SpookyJS.")
-            throw e;
-        }
+var setup, queue, visited, spooky, pullLinks, setup
+var maxpages = 10
+var currentpage = 0
 
-        spooky.start("http://en.wikipedia.org/wiki/Spooky_the_Tuff_Little_Ghost");
+var config = {
+    child: {
+        transport: 'http'
+    },
+    casper: {
+        clientScripts: [ "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"],
+        logLevel: 'debug',
+        verbose: true
+    }
+}
+
+//Start URL.
+queue = ["http://en.wikipedia.org/wiki/Spooky_the_Tuff_Little_Ghost"]
+visited = []
+spooky = null
+
+pullLinks = function (){
+    countAnchors = false;
+    visited = []
+    linkcount = 0;
+    link = 0
+    
+    spooky.start(queue[0])
+    spooky.then([{countAnchors: countAnchors}, function () {  
+        //Get ready to hand off to link scanner.
+        var urlsolver = require('url')
+        linkcount = 0
         
-        spooky.then(function () {
-            countAnchors = false;
-            maxpages = 3
-            currentpage = 0
-            queue = []
-            visited = []
-            linkcount = 0;
-            link = 0
+        winurl = this.evaluate(function(){return window.location.href})
+        do{
+            link = this.evaluate(function(index){return $("a[href]:eq("+index+")").attr("href")}, linkcount)
+            linkcount += 1;
+        } while (link && (link.indexOf("#") != -1 && !countAnchors))
+        //Skip anchors if flag is set.
+     
+        while (link){
+            this.emit("link", urlsolver.resolve(winurl, link) )
+            winurl = this.evaluate(function(){return window.location.href})
+            do{
+                link = this.evaluate(function(index){return $("a[href]:eq("+index+")").attr("href")}, linkcount)
+                linkcount += 1;
+            } while (link && (link.indexOf("#") != -1 && !countAnchors))
+            //Skip anchors if flag is set.
+            //Make the while statement true to skip the current link.
+        }
+     
+        this.emit('hello', "\nOn page: " + winurl + " Found: " + linkcount + " pages.");
+    }]);
 
-            while (currentpage < maxpages){
-                //Get ready to hand off to link scanner.
-                var urlsolver = require('url')
-                
-                winurl = this.evaluate(function(){return window.location.href})
-                do{
-                    link = this.evaluate(function(index){return $("a[href]:eq("+index+")").attr("href")}, linkcount)
-                    linkcount += 1;
-                } while (link && (link.indexOf("#") != -1 && !countAnchors))
-                //Skip anchors if flag is set.
+    return spooky.run()
+}
+
+
+setup = function (err) {
     
-                while (link){
-                    queue.push( urlsolver.resolve(winurl, link) )
-                    winurl = this.evaluate(function(){return window.location.href})
-                    do{
-                        link = this.evaluate(function(index){return $("a[href]:eq("+index+")").attr("href")}, linkcount)
-                        linkcount += 1;
-                    } while (link && (link.indexOf("#") != -1 && !countAnchors))
-                    //Skip anchors if flag is set.
-                }
+    if (err) {
+        e = new Error('Failed to initialize SpookyJS');
+        e.details = err;
+        console.log("Error, could not initialize SpookyJS.")
+        throw e;
+    }
+
+    spooky.on("link", function(url){
+        queue.push(url)
+    });
+
+    spooky.on('hello', function (greeting) {
+        console.log(greeting);
+        console.log("Number queued is now: " + queue.length + ". Next page " + queue[1] + "\n")
+    });
     
-                this.emit('hello', "\nOn page: " + winurl + " Found: " + linkcount + " pages. Number queued is now: " + queue.length + ". Next page " + queue[0] + "\n");
-                this.emit( 'hello', this.evaluate(function(nexturl){window.location.assign(nexturl); return window.location.href}, queue[0]) )
-                queue.shift()
-                currentpage++
-             }
-        });
-        spooky.run();
-    }
-);
+    spooky.on('error', function (e, stack) {
+        //console.error(e);
+        //if (stack) {console.log(stack);}
+    });
+
+    spooky.on('run.complete', function(){
+        currentpage += 1
+        queue.shift()
+        spooky.destroy();
+        spooky = new Spooky(config, setup);
+    })
+
+    return pullLinks();
+
+}
 
 
-spooky.on('hello', function (greeting) {
-    console.log(greeting);
-});
-
-
-spooky.on('error', function (e, stack) {
-    console.error(e);
-
-    if (stack) {
-        console.log(stack);
-    }
-});
-
+spooky = new Spooky(config, setup);
